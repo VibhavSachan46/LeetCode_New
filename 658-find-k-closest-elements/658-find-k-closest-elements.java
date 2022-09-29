b372dddfec97cedf9febf22d123a06687bba49b5
Vibhav Raj Sachan <95123263+VibhavSachan46@users.noreply.github.com>
2022-09-29 19:13:27 +0530

    Time: 6 ms (82.43%), Space: 61.5 MB (83.15%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,6 +1,5 @@
 class Solution {
-   public List<Integer> findClosestElements(int[] arr, int k, int x) {
-
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = 0, right = arr.length - k;
         while (left < right) {
             int midpoint = left + (right - left) / 2; // same as (left + right) / 2
