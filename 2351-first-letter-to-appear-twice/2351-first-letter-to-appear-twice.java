399eaa11f9557d5376b0cc1ccf8c3c4250d0fe5c
Vibhav Raj Sachan <95123263+VibhavSachan46@users.noreply.github.com>
2022-10-01 19:05:42 +0530

    Time: 1 ms (67.05%), Space: 42.8 MB (7.49%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashSet<Character> set=new HashSet<>();
+        
+        for(char x : s.toCharArray()){
+            if(set.contains(x)) 
+                return x;
+            else set.add(x);
+        }
+        return 'a';
+
+    }
+}
